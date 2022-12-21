120641c07b89075f353feedee328672d071490a9
Munir <dexamethazon@gmail.com>
2022-12-21 10:13:50 +0700

    Change config

--- src/IotBridgeServiceProvider.php
+++ src/IotBridgeServiceProvider.php
@@ -7,21 +7,13 @@ use Illuminate\Support\ServiceProvider;
 class IotBridgeServiceProvider extends ServiceProvider
 {
     public function boot() {
-        $this->publishes([
-            __DIR__.'/../config/config.php' => config_path('iotbridge.php'),
-        ]);
+        if ($this->app->runningInConsole()) {
+            $this->publishes([__DIR__ . '/../config' => config_path(), 'iotbridge-config']);
+        }
     }
 
     public function register() {
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/config.php', 'iotbridge'
-        );
-
-        $this->app->bind(IotBridge::class, function ($app) {
-            return new IotBridge(
-                $app->make('config')->get('iotbridge')
-            );
-        });
+        $this->mergeConfigFrom(__DIR__ . '/../config/iotbridge.php', 'config');
     }
     
 }
